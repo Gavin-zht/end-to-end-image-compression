160beb11e8a8150ae3929967fbfa2b4cbe3c5d1b
zhanght2120 <zhanght2120@163.com>
2024-12-29 19:38:57 +0800

    zht提交最新注释

--- model.py
+++ model.py
@@ -16,6 +16,13 @@ from models import *
 
 # zht test first commit
 def save_model(model, iter, name):
+    """_summary_
+    将训练迭代了iter轮的模型model进行保存，保存到name所指明的路径下
+    Args:
+        model (_type_): 模型对象
+        iter (_type_): 训练迭代次数
+        name (_type_): 
+    """
     torch.save(model.state_dict(), os.path.join(name, "iter_{}.pth.tar".format(iter)))
 
 
@@ -36,28 +43,47 @@ def load_model(model, f):
 
 
 class ImageCompressor(nn.Module):
+    """
+    图像压缩模型
+    继承自nn.Module类
+    
+    """
     def __init__(self, out_channel_N=128):
         super(ImageCompressor, self).__init__()
-        self.Encoder = Analysis_net_17(out_channel_N=out_channel_N)
-        self.Decoder = Synthesis_net_17(out_channel_N=out_channel_N)
-        self.bitEstimator = BitEstimator(channel=out_channel_N)
-        self.out_channel_N = out_channel_N
+        self.Encoder = Analysis_net_17(out_channel_N=out_channel_N) # 分析变换网络作为模型的编码器Encoder
+        self.Decoder = Synthesis_net_17(out_channel_N=out_channel_N) # 合成变换网络作为模型的解码器Decoder
+        self.bitEstimator = BitEstimator(channel=out_channel_N) 
+        self.out_channel_N = out_channel_N  # 图像压缩模型的输出通道数
 
     def forward(self, input_image):
-        quant_noise_feature = torch.zeros(input_image.size(0), self.out_channel_N, input_image.size(2) // 16, input_image.size(3) // 16).cuda()
-        quant_noise_feature = torch.nn.init.uniform_(torch.zeros_like(quant_noise_feature), -0.5, 0.5)
-        feature = self.Encoder(input_image)
-        batch_size = feature.size()[0]
+        """_summary_
+
+        Args:
+            input_image : 输入图像， 维度为: (batch_size, Channels, height, width)
+
+        Returns:
+            _type_: _description_
+        """
+        quant_noise_feature = torch.zeros(input_image.size(0), self.out_channel_N, input_image.size(2) // 16, input_image.size(3) // 16).cuda() 
+        #* quant_noise_feature: 一个全零张量，用于存储量化噪声特征，维度为[batch_size, Channels, Height/16, Width/16]
+        quant_noise_feature = torch.nn.init.uniform_(torch.zeros_like(quant_noise_feature), -0.5, 0.5)  #* 初始化quant_noise_feature为均匀分布的噪声, quant_noise_feature的维度为[batch_size, Channels, Height/16, Width/16]
+        feature = self.Encoder(input_image) #* feature 表示 编码器的输出，i.e. 隐变量y
+        batch_size = feature.size()[0]  # batch_size 表示 小批量的样本数量
         feature_renorm = feature
         if self.training:
+            #* 如果此时模型处于训练状态，那么就用 (feature_renorm + quant_noise_feature) 作为 量化值
             compressed_feature_renorm = feature_renorm + quant_noise_feature
         else:
+            #* 如果此时模型处于此时状态，那么就用 torch.round(feature_renorm) 作为 量化值
             compressed_feature_renorm = torch.round(feature_renorm)
-        recon_image = self.Decoder(compressed_feature_renorm)
+            
+        #* 至此，compressed_feature_renorm 表示 经过量化后的编码值。
+        #* 经过量化的编码值被传送给解码器Decoder    
+        recon_image = self.Decoder(compressed_feature_renorm) #* 解码器用量化后的编码值作为输入，得到重建图像
         # recon_image = prediction + recon_res
         clipped_recon_image = recon_image.clamp(0., 1.)
         # distortion
-        mse_loss = torch.mean((recon_image - input_image).pow(2))
+        mse_loss = torch.mean((recon_image - input_image).pow(2))   #* 使用MSE计算原始图像和重建图像之间的差距，得到失真distortion
 
         # def feature_probs_based_sigma(feature, sigma):
         #     mu = torch.zeros_like(sigma)
